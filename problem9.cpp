8d68fec2f5db006df52ccd1cd82457657eeec09b
ELBEK9 <70363038+ELBEK9@users.noreply.github.com>
2024-03-15 18:02:01 +0500

    Update problem9.cpp

--- problem9.cpp
+++ problem9.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 bool compareStudents(pair<int, int> s1, pair<int, int> s2) {
-    if(s1.second == s2.second) {
+    if(s1.second==s2.second) {
         return s1.first < s2.first;
     }
     return s1.second > s2.second;
@@ -17,7 +17,6 @@ int main()
     vector<pair<int, int>> students(n);
     for(int i=0; i<n ;i++) {
         students[i].first = i + 1; 
-        cout<<"Enter the grades of student "<<students[i].first<<": ";
         cin>>students[i].second;
         for(int j=0; j<2 ;j++) {
             int grade;
@@ -25,8 +24,9 @@ int main()
             students[i].second=students[i].second + grade;
         }
     }
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
+    
+    for(int i=0; i<n-1; i++) {
+        for (int j=0; j<n-i-1; j++) {
             if (!compareStudents(students[j], students[j + 1])) {
                 swap(students[j], students[j + 1]);
             }
@@ -35,7 +35,7 @@ int main()
     
     int rank = 1;
     for(auto student:students) {
-        if (student.first == 2) {
+        if(student.first==2) {
             break;
         }
         rank++;
